98454bd8ffc3ca2217074597402a5c2bdb2beca8
yuanrui01 <xdyrfree@gmail.com>
2024-10-01 15:56:19 +0800

    java响应式风格学习

---
+++ src/main/java/org/hypnos/flow/ExceptionTest.java
@@ -0,0 +1,52 @@
+package org.hypnos.flow;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author: yuanrui
+ */
+public class ExceptionTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        getInts()
+                .flatMap(integer -> {
+                    try {
+                        return i2s(integer);
+                    } catch (Exception e) {
+                        return Mono.error(new RuntimeException("发生了大事了！！！！"));
+                    }
+                })
+                .onErrorContinue((e, value) -> {
+                    System.out.println("error message: " + e.getMessage());
+                })
+                .doOnError(e -> System.out.println(e.getMessage()))
+                .subscribe(ExceptionTest::printEle);
+
+        // Thread.sleep(100000);
+        System.out.println("main ending");
+    }
+
+
+    public static Flux<Integer> getInts() {
+        List<Integer> integers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8));
+
+        return Flux.fromIterable(integers);
+    }
+
+    public static Mono<String> i2s(Integer i) {
+        if (i == 5)
+            throw new RuntimeException();
+        return Mono.just(String.format("Random:%s, Integer:%s", UUID.randomUUID(), i));
+    }
+
+    private static void printEle(String s) {
+        System.out.println(s);
+    }
+}
--- src/main/java/org/hypnos/flow/PubSubTest.java
+++ src/main/java/org/hypnos/flow/PubSubTest.java
@@ -1,57 +1,57 @@
-package org.hypnos.flow;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Flow;
-import java.util.concurrent.SubmissionPublisher;
-
-/**
- * @author: yuanrui
- */
-public class PubSubTest {
-
-    public static void main(String[] args) throws InterruptedException {
-        // 创建一个 CountDownLatch，以便主线程可以等待数据处理完成
-        CountDownLatch latch = new CountDownLatch(1);
-
-        // 创建一个发布者
-        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
-
-        // 创建一个订阅者
-        Flow.Subscriber<String> subscriber = new Flow.Subscriber<>() {
-            @Override
-            public void onSubscribe(Flow.Subscription subscription) {
-                subscription.request(Long.MAX_VALUE); // 请求所有数据
-            }
-
-            @Override
-            public void onNext(String item) {
-                System.out.println("Received: " + item);
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                throwable.printStackTrace();
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("Completed");
-                // 完成后减小计数器，允许主线程结束
-                latch.countDown();
-            }
-        };
-
-        // 将订阅者注册到发布者
-        publisher.subscribe(subscriber);
-
-        // 发布数据
-        publisher.submit("Hello");
-        publisher.submit("World");
-
-        // 关闭发布者
-        publisher.close();
-
-        // 等待直到所有数据都被处理完成
-        latch.await();
-    }
-}
+//package org.hypnos.flow;
+//
+//import java.util.concurrent.CountDownLatch;
+//import java.util.concurrent.Flow;
+//import java.util.concurrent.SubmissionPublisher;
+//
+///**
+// * @author: yuanrui
+// */
+//public class PubSubTest {
+//
+//    public static void main(String[] args) throws InterruptedException {
+//        // 创建一个 CountDownLatch，以便主线程可以等待数据处理完成
+//        CountDownLatch latch = new CountDownLatch(1);
+//
+//        // 创建一个发布者
+//        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
+//
+//        // 创建一个订阅者
+//        Flow.Subscriber<String> subscriber = new Flow.Subscriber<>() {
+//            @Override
+//            public void onSubscribe(Flow.Subscription subscription) {
+//                subscription.request(Long.MAX_VALUE); // 请求所有数据
+//            }
+//
+//            @Override
+//            public void onNext(String item) {
+//                System.out.println("Received: " + item);
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//                throwable.printStackTrace();
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                System.out.println("Completed");
+//                // 完成后减小计数器，允许主线程结束
+//                latch.countDown();
+//            }
+//        };
+//
+//        // 将订阅者注册到发布者
+//        publisher.subscribe(subscriber);
+//
+//        // 发布数据
+//        publisher.submit("Hello");
+//        publisher.submit("World");
+//
+//        // 关闭发布者
+//        publisher.close();
+//
+//        // 等待直到所有数据都被处理完成
+//        latch.await();
+//    }
+//}
